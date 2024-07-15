776f2d11c4bb429f4011a2975ca8d8b018c470c0
Esteban Gimbernat <egimbernat@pm.me>
2024-07-15 09:28:19 +0200

    Quiz added

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const questionForm = document.getElementById('question-form');
 const modeSelection = document.getElementById('mode-selection');
 const adminContainer = document.getElementById('admin-container');
 
-let questions = shuffleArray(JSON.parse(localStorage.getItem('questions')) || [
+//let questions = shuffleArray(JSON.parse(localStorage.getItem('questions')) ||
+
+let questions = shuffleArray( [
         {
             "question": "Con rotor antihorario cuando aumenta la velocidad la sustentacion translacional es:",
             "answers": ["Es efectiva sube el morro y tiende a girar a izquierda", "Es efectiva sube el morro y alabea a la derecha", "Es menos efectiva baja el morro y a la derecha", "No afecta"],
@@ -229,7 +231,222 @@ let questions = shuffleArray(JSON.parse(localStorage.getItem('questions')) || [
             "question": "Durante una autorrotación un helicóptero acelera de 60 a 90 nudos sin más correcciones. Las RPM del rotor...",
             "answers": ["variar", "disminuir", "aumentar", "permanecer constante"],
             "correct": 1
-        }
+        },
+        {
+            "question": "Donde se siente la vibración de alta frecuencia del rotor de cola:",
+            "answers": ["Pedales", "Cíclico", "Colectivo", "Techo"],
+            "correct": 1
+        },
+    {
+        "question": "En vuelo hacia adelante, la pala de retirada experimenta una inversión del flujo de aire que:",
+        "answers": [
+            "Aumenta la tendencia del disco a batir hacia atrás",
+            "Disminuye la resistencia del rotor en la pala de avance",
+            "Aumenta la resistencia del rotor en la pala de retirada",
+            "Aumenta la tendencia del disco a batir hacia adelante"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "¿Qué sucede cuando ocurre un sobrepaso?",
+        "answers": [
+            "Las RPM del rotor disminuyen y el ángulo de cono disminuye",
+            "Las RPM del rotor aumentan y el ángulo de cono aumenta",
+            "Las RPM del rotor disminuyen y el ángulo de cono aumenta",
+            "Las RPM del rotor aumentan y el ángulo de cono disminuye"
+        ],
+        "correct": 2
+    },
+    {
+        "question": "¿Con qué se asocia la inversión del flujo de aire?",
+        "answers": [
+            "Vuelo a alta velocidad hacia adelante, originándose en la raíz de la pala de retirada",
+            "Vuelo a baja velocidad hacia adelante, originándose en la punta de la pala de retirada",
+            "Vuelo a alta velocidad hacia adelante, originándose en la punta de la pala de retirada",
+            "Vuelo a baja velocidad hacia adelante, originándose en la raíz de la pala de retirada"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "¿A qué altura del patín es más efectivo el efecto suelo?",
+        "answers": [
+            "Sobre hierba alta",
+            "A 4 pies",
+            "Alrededor de 3/4 del diámetro del rotor",
+            "Por debajo de 4 pies"
+        ],
+        "correct": 3
+    },
+    {
+        "question": "¿Por qué la velocidad máxima durante el vuelo hacia atrás es menor que en el vuelo hacia adelante?",
+        "answers": [
+            "el rango de control se reduce en la dirección hacia atrás, junto con la estabilidad direccional del fuselaje",
+            "interferencia aerodinámica entre el rotor principal y el rotor de cola",
+            "el espacio restringido entre el rotor principal y el rotor de cola",
+            "el aumento del arrastre parásito al volar hacia atrás"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "Durante la autorrotación, un aumento en el paso colectivo ____ la fuerza del rotor, ____ las RPM del rotor y ____ la tasa de descenso.",
+        "answers": [
+            "aumenta; disminuye; disminuye",
+            "disminuye; disminuye; aumenta",
+            "aumenta; aumenta; aumenta",
+            "disminuye; disminuye; disminuye"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "¿Con qué se asocia el flujo de aire invertido sobre parte del disco del rotor?",
+        "answers": [
+            "vuelo a alta velocidad hacia adelante",
+            "vórtice de anillo",
+            "autorrotación",
+            "flotando en efecto suelo"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "La potencia del perfil del rotor es la requerida para:",
+        "answers": [
+            "Mantener las RPM del rotor con la palanca colectiva completamente hacia abajo y para accionar el rotor de cola y los accesorios",
+            "Contrarrestar el arrastre inducido",
+            "Contrarrestar el arrastre parásito",
+            "Inducir un flujo"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "¿Qué es el Centro de Presión en relación con una sección de aerofolio?",
+        "answers": [
+            "Donde actúa la reacción total",
+            "Donde el flujo de aire se separa de la sección del aerofolio",
+            "Donde ocurre el espesor máximo del aerofolio",
+            "Donde actúa el peso total de la aeronave"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "¿Cuál es el propósito del rango negativo del paso del rotor de cola?",
+        "answers": [
+            "Se usa para el control de guiñada en autorrotación",
+            "para eliminar el aleteo del rotor de cola",
+            "para contrarrestar la deriva del rotor de cola",
+            "para el control al realizar un giro puntual a favor del torque"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "Durante un descenso autorrotativo, la distancia de planeo máxima se obtendrá a:",
+        "answers": [
+            "una velocidad mayor que la asociada con la tasa mínima de descenso",
+            "una velocidad ligeramente inferior a la asociada con la tasa mínima de descenso",
+            "la velocidad asociada con la tasa mínima de descenso",
+            "la velocidad mínima que mantendrá las RPM del rotor"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "¿Por qué se determina el ángulo de cono?",
+        "answers": [
+            "La sustentación y la fuerza centrífuga producida por una pala",
+            "La sustentación y el arrastre producido por una pala",
+            "La fuerza centrífuga y las RPM del rotor",
+            "El empuje del rotor y el arrastre del rotor"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "¿Qué tres características principales definen la velocidad máxima de un helicóptero?",
+        "answers": [
+            "el límite de compresibilidad de la pala de avance, el bloqueo de la pala de retirada y los límites de diseño del control cíclico",
+            "el límite de RPM del rotor, la velocidad de retroceso y la inversión del flujo de aire",
+            "la resistencia de la pala de retirada, el límite de velocidad de la pala y el límite de velocidad del rotor de cola",
+            "el límite de diseño del control colectivo, la compresibilidad de las palas de avance y la inversión del flujo de aire"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "¿Qué es cierto respecto al Estado de Vórtice de Anillo?",
+        "answers": [
+            "Es una condición inestable que puede resultar en una tasa de descenso incontrolada",
+            "Es una condición estable que reduce la tasa de descenso",
+            "Es una condición estable que aumenta la tasa de descenso",
+            "Normalmente se controla aumentando el paso colectivo"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "Si se aumenta el ángulo de cono:",
+        "answers": [
+            "el centro de gravedad de la pala se desplaza hacia adentro",
+            "el centro de gravedad del helicóptero se desplaza hacia adelante",
+            "el centro de gravedad del helicóptero se desplaza hacia atrás",
+            "el centro de gravedad de la pala se desplaza hacia afuera"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "El ángulo de ataque de una pala de rotor de helicóptero es el ángulo entre:",
+        "answers": [
+            "la línea de cuerda de la pala y el flujo de aire relativo",
+            "la línea de cuerda de la pala y el flujo de aire invertido",
+            "la línea de cuerda de la pala y el plano de rotación",
+            "la superficie inferior de la pala y el plano de trayectoria de la punta"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "Durante la autorrotación en un helicóptero con rotación en sentido antihorario visto desde arriba, el empuje del rotor de cola:",
+        "answers": [
+            "Se dirige hacia la izquierda cuando se ve desde atrás",
+            "Es mayor que en el vuelo normal con motor",
+            "Actúa en la misma dirección que en el vuelo normal con motor",
+            "Se dirige hacia la derecha cuando se ve desde atrás"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "¿A qué se debe la resonancia del suelo?",
+        "answers": [
+            "una acoplamiento entre la frecuencia de un desequilibrio del rotor principal y la frecuencia natural del tren de aterrizaje",
+            "una frecuencia alta y una vibración de baja amplitud que emana de áreas de piel delgada como puertas y paneles",
+            "la estela del rotor principal que afecta al rotor de cola cuando está en el suelo o cerca de él",
+            "una velocidad de balanceo que no puede ser detenida con el ciclo completo opuesto"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "En vuelo hacia adelante, si las RPM del rotor aumentan por encima de los límites especificados:",
+        "answers": [
+            "las grandes fuerzas centrífugas imponen cargas severas y posiblemente excesivas en el buje",
+            "el aumento de la fuerza centrífuga disminuye el ángulo de cono y reduce las RPM a su nivel original",
+            "la mayor resistencia de la pala automáticamente hace que las RPM se reduzcan a su nivel original",
+            "se inducirá una pérdida de la pala de retirada a una velocidad reducida hacia adelante"
+        ],
+        "correct": 0
+    },
+    {
+        "question": "El empuje total del rotor de un helicóptero...",
+        "answers": [
+            "está en ángulo recto con el plano de rotación, actuando a través del buje.",
+            "actúa a través del eje del eje.",
+            "está en ángulo recto con el plano de rotación, pasando por el centro de gravedad.",
+            "actúa a través del centro de presión de cada pala."
+        ],
+        "correct": 0
+    },
+    {
+        "question": "Cual es la secuencia correcta al entrar en autorotacion",
+        "answers": [
+            "Levantar colectivo y empujar morro hacia abajo",
+            "Levantar colecyivo y hacer flare",
+            "Bajar colectivo y hacer flare",
+            "Bajar colectivo y empujar morro hacia abajo",
+        ],
+        "correct": 2
+    },
     ]);
 
 let currentQuestionIndex = 0;
